33ffbf68e742dbe36ae4b03cefcb8a6b598d775f
ravikumar <ravipersonalemail@gmail.com>
2024-11-10 20:49:35 +0530

    added careers page and contact us page

--- src/pages/Home/components/Careers.tsx
+++ src/pages/Home/components/Careers.tsx
@@ -1,4 +1,5 @@
-import { Box, styled, Typography } from '@mui/material';
+import { Mail } from '@mui/icons-material';
+import { Box, Button, styled, Typography } from '@mui/material';
 import React from 'react'
 
 const StyledCareersContainer = styled(Box)`
@@ -17,13 +18,86 @@ const StyledTitle = styled(Typography)`
   }
 `;
 
+const ContentImageWrapper = styled(Box)`
+  margin-top:30px;
+  padding-left: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px; 
+  @media (max-width: 900px) {
+    padding-left: 25px;
+    gap: 15px;
+  }
+`;
+
+const StyledContent = styled(Typography)`
+  font-size: 24px;
+  color: black;
+  @media (max-width: 900px) {
+    font-size: 15px;
+  }
+`;
+
+const ImageContainer = styled(Box)`
+  width: 75%;
+  display: block;
+  @media (max-width: 900px) {
+    width: 80%;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: 20px;
+  margin-left:auto;
+  margin-right:auto;
+  background-color: #7A6CCB;
+  color: white;
+  width:250px;
+  height:50px;
+  dispaly:flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledLinkButton = styled('a')`
+  margin-top: 20px;
+  margin-left:auto;
+  margin-right:auto;
+  padding: 10px 30px;
+  background-color: #7A6CCB;
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+  text-align: center;
+  border-radius:5px;
+  @media(max-width:900px){
+    padding:5px 20px;
+  }
+`;
 const Careers = () => {
   return (
     <StyledCareersContainer id="careers">
       <StyledTitle>Careers</StyledTitle>
-      <Typography fontWeight={900} fontSize='18px'>Everyone has an opportunity to grow in their career through our Kovalty </Typography>
+      <ContentImageWrapper>
+        <StyledContent>Everyone has an opportunity to grow in their career through our Kovalty</StyledContent>
+        <ImageContainer>
+          <Box
+            component="img"
+            src="/assets/Carrers_Img.png"
+            alt="Kovalty Technologies"
+            style={{ height: "100%", width: "100%" }}
+          />
+        </ImageContainer>
+        {/* <StyledButton variant="contained">
+          Explore Careers
+          </StyledButton> */}
+        <StyledLinkButton href="mailto:info@kovalty.com">
+          Explore Carrers
+        </StyledLinkButton>
+      </ContentImageWrapper>
+
     </StyledCareersContainer>
-  )
+  );
 }
 
-export default Careers
+export default Careers;
